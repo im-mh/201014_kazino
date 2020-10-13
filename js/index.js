69675582bf47dc3f31ef28fc96dd2be109fcb861
im-mh <62644451+im-mh@users.noreply.github.com>
2020-10-14 06:55:16 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$(".remove-button").on("click", function () {
+		$(".black-box").removeClass("black-box")
+		$(".img3").addClass("rotation3")
+		$(".img4").addClass("rotation4")
+		$(".img5").addClass("rotation5")
+		$(".img6").addClass("rotation6")
+
+	
+	})
+
+})
